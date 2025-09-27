9514fd00e18af9ee8767875b3a72b4dfb629675b
Ewan-Dev <ewanmccairn2010@gmail.com>
2025-09-27 18:33:41 +0100

    feat(morse): add morse decipher

--- ciphers.py
+++ ciphers.py
@@ -233,3 +233,38 @@ def morse_encipher(ciphertext):
             plaintext += '?'
     
     return plaintext
+
+
+def morse_decipher(ciphertext):
+    morse_code = {
+    # Letters
+    'A': ".-",    'B': "-...",  'C': "-.-.",  'D': "-..",
+    'E': ".",     'F': "..-.",  'G': "--.",   'H': "....",
+    'I': "..",    'J': ".---",  'K': "-.-",   'L': ".-..",
+    'M': "--",    'N': "-.",    'O': "---",   'P': ".--.",
+    'Q': "--.-",  'R': ".-.",   'S': "...",   'T': "-",
+    'U': "..-",   'V': "...-",  'W': ".--",   'X': "-..-",
+    'Y': "-.--",  'Z': "--..",
+
+    # Numbers
+    '0': "-----", '1': ".----", '2': "..---", '3': "...--",
+    '4': "....-", '5': ".....", '6': "-....", '7': "--...",
+    '8': "---..", '9': "----.",
+
+    # Common symbols
+    '.': ".-.-.-",   ',': "--..--",   '?': "..--..",  "'": ".----.",
+    '!': "-.-.--",   '/': "-..-.",    '(': "-.--.",   ')': "-.--.-",
+    '&': ".-...",    ':': "---...",   ';': "-.-.-.",  '=': "-...-",
+    '+': ".-.-.",    '-': "-....-",   '_': "..--.-",  '"': ".-..-.",
+    '$': "...-..-",  '@': ".--.-.",   ' ': '/'        
+}
+    morse_code_decipher = {morse: char for char, morse in morse_code.items()}
+    ciphertext_list = ciphertext.split(" ")
+    plaintext = ""
+    for morse in ciphertext_list:
+        if morse in morse_code_decipher:
+            plaintext += morse_code_decipher[morse]
+        else:
+            plaintext += '?'
+    
+    return plaintext
